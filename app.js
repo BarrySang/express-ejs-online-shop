2bba93b98bbbe7980de5d2f2709a8de19b07da85
Barry Sang <barrysang8@gmail.com>
2024-03-12 10:40:21 +0300

    add about us page and navbar

--- app.js
+++ app.js
@@ -6,35 +6,55 @@ const port = process.env.PORT || 5000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-const items = generateItems(10)
-console.log(items)
+const products = generateProducts(10)
+console.log(products)
+
+const data = {
+    products: [],
+    content: ''
+}
 
 /**
- * root route
+ * routes
  */
+// root
 app.get('/', (req, res) => {
-    res.render('index', {data: 'updated data from server'})
+    sentData = {
+        ...data,
+        products: products,
+        content: 'products'
+    }
+    res.render('index', {sentData})
+})
+
+// about
+app.get('/about', (req, res) => {
+    sentData = {
+        ...data,
+        content: 'about'
+    }
+    res.render('index', {sentData})
 })
 
 // @TODO - move this function to 'lib' folder
 /**
  * 
- * @param {number} numberOfItems 
+ * @param {number} numberOfProducts 
  * @returns array
  */
- function generateItems(numberOfItems) {
-    let items = []
-    for (let i = 0; i < numberOfItems; i++) {
-        const item = {
-            id: items.length ? items[items.length - 1].id + 1 : 1,
-            itemName: items.length ? 'item'+(items[items.length - 1].id + 1) : 'item1',
+ function generateProducts(numberOfProducts) {
+    let products = []
+    for (let i = 0; i < numberOfProducts; i++) {
+        const product = {
+            id: products.length ? products[products.length - 1].id + 1 : 1,
+            productName: products.length ? 'product'+(products[products.length - 1].id + 1) : 'product1',
             price: Math.floor(Math.random() * 10000)+500,
             inStock: Math.floor(Math.random() * 200),
         }
-        items.push(item)
+        products.push(product)
     }
 
-    return items
+    return products
 }
 
 app.listen(port, () => console.log(`listening on port ${port}`))
